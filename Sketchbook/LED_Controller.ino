8def409faf03da46db8e578d03f2c513265b8fe6
Alchemy Meister <jesusesma@gmail.com>
2015-11-10 23:38:10 +0100

    implemented the distinction between fading and breathing on Arduino.

--- Sketchbook/LED_Controller.ino
+++ Sketchbook/LED_Controller.ino
@@ -1,5 +1,7 @@
 // LED_Controller.ino
 
+#include <math.h>
+
 const uint8_t RED_PIN = 3;
 const uint8_t GREEN_PIN = 5;
 const uint8_t BLUE_PIN = 6;
@@ -12,13 +14,14 @@ const uint8_t READ_RED = 'C';
 const uint8_t READ_GREEN = 'D';
 const uint8_t READ_BLUE = 'E';
 
-const uint8_t BREATHING = 'H';
-const uint8_t SPECTRUM_CYCLING = 'I';
-const uint8_t STATIC = 'J';
+const uint8_t FADE = 'H';
+const uint8_t BREATHING = 'I';
+const uint8_t SPECTRUM_CYCLING = 'J';
+const uint8_t STATIC = 'K';
 
 uint8_t redLedValue = 0;
 float redLedCurrentValue;
-uint8_t redLedFadeSpeed;		//Speed equals the RGB color divided by the duration of the animation in seconds.
+uint8_t redLedFadeSpeed;			//Speed equals the RGB color divided by the duration of the animation in seconds.
 
 uint8_t greenLedValue = 0;
 float greenLedCurrentValue;
@@ -31,7 +34,9 @@ uint8_t blueLedFadeSpeed;
 uint8_t currentEffect = STATIC;
 
 uint8_t fadeIn = 0;
-float fadeDuration = 2000000.0; 			//Time of the fade animation in microseconds.
+uint8_t breathing = 0;
+float fadeDuration = 3000000.0; 	//Time of the fade animation in microseconds.
+float offDuration = 1500000.0;
 float fadeDurationSeconds = fadeDuration / 1000000;
 unsigned long fadeStart;
 unsigned long fadeElapsedTime;
@@ -111,17 +116,26 @@ void process(void) {
 		case READ_RED: case READ_GREEN: case READ_BLUE:
 			readPin(code[0]);
 			break;
-		case BREATHING: case SPECTRUM_CYCLING: case STATIC:
-			currentEffect = code[0];
+		case BREATHING: case FADE:
+			currentEffect = FADE;
 			
 			redLedCurrentValue = redLedValue;
 			greenLedCurrentValue = greenLedValue;
 			blueLedCurrentValue = blueLedValue;
 
-			redLedFadeSpeed = redLedCurrentValue / fadeDurationSeconds;
-			greenLedFadeSpeed = greenLedCurrentValue / fadeDurationSeconds;
-			blueLedFadeSpeed = blueLedCurrentValue / fadeDurationSeconds;
+			redLedFadeSpeed = ceil(redLedCurrentValue / fadeDurationSeconds);
+			greenLedFadeSpeed = ceil(greenLedCurrentValue / fadeDurationSeconds);
+			blueLedFadeSpeed = ceil(blueLedCurrentValue / fadeDurationSeconds);
 			
+			if(code[0] == FADE)
+				breathing = 0;
+			else
+				breathing = 1;
+
+			reset();
+			break;
+		case SPECTRUM_CYCLING: case STATIC:
+			currentEffect = code[0];
 			reset();
 			break;
 	}
@@ -129,12 +143,21 @@ void process(void) {
 
 void processEffect(void) {
 	switch(currentEffect) {
-		case BREATHING:
+		case FADE:
 			fadeElapsedTime = micros() - fadeStart;
+			
 			if(fadeIn) {
 				redLedCurrentValue += redLedFadeSpeed * deltaTime;
 				greenLedCurrentValue += greenLedFadeSpeed * deltaTime;
 				blueLedCurrentValue += blueLedFadeSpeed * deltaTime;
+				
+				if(redLedCurrentValue > 255)
+					redLedCurrentValue = 255;
+				if(greenLedCurrentValue > 255)
+					greenLedCurrentValue = 255;
+				if(blueLedCurrentValue > 255)
+					blueLedCurrentValue = 255;
+
 				if(fadeElapsedTime >= fadeDuration) {
 					fadeIn = 0;
 					fadeStart = micros();
@@ -143,14 +166,37 @@ void processEffect(void) {
 				redLedCurrentValue -= redLedFadeSpeed * deltaTime;
 				greenLedCurrentValue -= greenLedFadeSpeed * deltaTime;
 				blueLedCurrentValue -= blueLedFadeSpeed * deltaTime;
+				
+				if(redLedCurrentValue < 0)
+					redLedCurrentValue = 0;
+				if(greenLedCurrentValue < 0)
+					greenLedCurrentValue = 0;
+				if(blueLedCurrentValue < 0)
+					blueLedCurrentValue = 0;
+
 				if(fadeElapsedTime >= fadeDuration) {
-					fadeIn = 1;
-					fadeStart = micros();
+					if(breathing) {
+						currentEffect = BREATHING;
+						fadeStart = micros();
+					} else {
+						fadeIn = 1;
+						fadeStart = micros();
+					}
 				}
 			}
+
 			analogWrite(RED_PIN, redLedCurrentValue);
 			analogWrite(GREEN_PIN, greenLedCurrentValue);
 			analogWrite(BLUE_PIN, blueLedCurrentValue);
+			break;
+		case BREATHING:
+			fadeElapsedTime = micros() - fadeStart;
+
+			if(fadeElapsedTime >= offDuration) {
+				currentEffect = FADE;
+				fadeIn = 1;
+				fadeStart = micros();
+			}
 			break;
 		case SPECTRUM_CYCLING:
 			
@@ -169,6 +215,7 @@ void loop() {
 			process();
 		}
 	}
+
 	lastTime = now;
 	now = micros();
 	deltaTime = (now - lastTime) / 1000000.0;
