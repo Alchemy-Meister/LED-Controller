b6d0f6cd75813d4d3d8c5d0138dfe1ac6d969f17
Alchemy-Meister <jesusesma@gmail.com>
2016-02-21 21:47:19 +0100

    Added a ON/OFF combo box the GUI.

--- LED Controller/SerialModel.cs
+++ LED Controller/SerialModel.cs
@@ -9,6 +9,13 @@
         public const string COM = "COM";
         public const short BaudRate = 9600;
 
+        private readonly Dictionary<string, byte> statusList =
+           new Dictionary<string, byte>()
+           {
+                { "On", Convert.ToByte('N') },
+                { "Off", Convert.ToByte('F') }
+           };
+
         private readonly Dictionary<string, byte> effectList =
             new Dictionary<string, byte>()
             {
@@ -18,11 +25,62 @@
                 { "Spectrum cycling", Convert.ToByte('J') }
             };
 
+        private bool ledPower = true;
+
+        private byte redLedValue = 0;
+        private byte greenLedValue = 0;
+        private byte blueLedValue = 0;
+
         private short serialPortNumber = -1;
+
+        public bool GetLedPower()
+        {
+            return this.ledPower;
+        }
+
+        public void SetLedPower(bool power)
+        {
+            this.ledPower = power;
+        }
 
         public Dictionary<string, byte> GetEffectList()
         {
             return this.effectList;
+        }
+
+        public Dictionary<string, byte> GetStatusList()
+        {
+            return this.statusList;
+        }
+
+        public byte GetRedLedValue()
+        {
+            return this.redLedValue;
+        }
+
+        public void SetRedLedValue(byte value)
+        {
+            this.redLedValue = value;
+        }
+
+        public byte GetGreenLedValue()
+        {
+            return this.greenLedValue;
+        }
+
+        public void SetGreenLedValue(byte value)
+        {
+            this.greenLedValue = value;
+        }
+
+        public byte GetBlueLedValue()
+        {
+            return this.blueLedValue;
+        }
+
+        public void SetBlueLedValue(byte value)
+        {
+            this.blueLedValue = value;
         }
 
         public short GetSerialPortNumber()
@@ -34,6 +92,5 @@
         {
             this.serialPortNumber = portNumber;
         }
-
     }
 }
